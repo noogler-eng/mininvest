c9d69c0974cf859bccb3b011f2f0bcdd64c337dc
noogler-eng <sharadpoddar10102003@gmail.com>
2026-04-05 19:08:33 +0530

    WIP

--- src/main/java/com/minivest/mutualfunds/service/impl/UserServiceImpl.java
+++ src/main/java/com/minivest/mutualfunds/service/impl/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.minivest.mutualfunds.service.impl;
-
-import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import com.minivest.mutualfunds.dto.RegisterUserRequest;
 import com.minivest.mutualfunds.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
-    
+    public void createUser(RegisterUserRequest req){
+
+    }
+
+    public void getUser(Long userId){
+        
+    }
+
+    public void getBankFromIfsc(String ifsc){
+
+    }
 }
